1e8bbd8d9b49358eb6593f8e1f432fafb829e3d9
ququzone <xueping.yang@gmail.com>
2022-10-27 15:29:12 +0800

    stage code

---
+++ src/MetapebbleContractFactory.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./interface/IMetapebbleContractFactory.sol";
+
+contract MetapebbleContractFactory is IMetapebbleContractFactory {
+    address public override validator;
+
+    
+}

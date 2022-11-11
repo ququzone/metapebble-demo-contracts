9741945fa2204c78880c97fd87f3d669c75164b0
ququzone <xueping.yang@gmail.com>
2022-11-11 11:38:49 +0800

    add sbt

---
+++ contracts/examples/PebbleOwnSBT.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "../MetapebbleVerifiedSBT.sol";
+
+contract PebbleOwnSBT is ReentrancyGuard, MetapebbleVerifiedSBT {
+    uint256 private lat;
+    uint256 private long;
+    uint256 private maxDistance;
+
+    constructor(
+        address _verifier,
+        string memory _name,
+        string memory _symbol
+    ) MetapebbleVerifiedSBT(_verifier, _name, _symbol) {}
+
+    function claim(
+        bytes32 deviceHash_,
+        uint256 deviceTimestamp_,
+        uint256 verifyTimestamp_,
+        bytes memory signature
+    ) external nonReentrant {
+        // own pebble verify logic
+        require(_claimedDevices[deviceHash_] == address(0), "already claimed");
+
+        _claim(msg.sender, deviceHash_, deviceTimestamp_, verifyTimestamp_, signature);
+    }
+}
---
+++ test/PebbleOwnSBT.spec.ts
@@ -0,0 +1,59 @@
+import { ethers } from "hardhat"
+import { expect } from "chai"
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
+
+import { MetapebbleDataVerifier } from "../typechain/contracts/MetapebbleDataVerifier"
+import { PebbleOwnSBT } from "../typechain/contracts/examples/PebbleOwnSBT"
+
+describe("PebbleOwnSBT", function () {
+    let verifier: MetapebbleDataVerifier
+    let token: PebbleOwnSBT
+    let owner: SignerWithAddress
+    let signer: SignerWithAddress
+    let holder: SignerWithAddress
+
+    before(async function () {
+        ;[owner, signer, holder] = await ethers.getSigners()
+
+        const verifierFactory = await ethers.getContractFactory("MetapebbleDataVerifier")
+        verifier = (await verifierFactory.connect(owner).deploy()) as MetapebbleDataVerifier
+        await verifier.initialize(signer.address)
+
+        const facory = await ethers.getContractFactory("PebbleOwnSBT")
+        token = (await facory
+            .connect(owner)
+            .deploy(verifier.address, "Own Pebble NFT", "OPT")) as PebbleOwnSBT
+    })
+
+    it("check basic info", async function () {
+        expect("Own Pebble NFT").to.equal(await token.name())
+        expect("OPT").to.equal(await token.symbol())
+    })
+
+    it("check claim", async function () {
+        const deviceHash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("12345"))
+        const hash = ethers.utils.solidityKeccak256(
+            ["address", "bytes32", "uint256", "uint256"],
+            [holder.address, deviceHash, 1668131000, 1668132000]
+        )
+        const messageHashBinary = ethers.utils.arrayify(hash)
+
+        const signature = await signer.signMessage(messageHashBinary)
+
+        await expect(
+            token.connect(owner).claim(deviceHash, 1668131000, 1668132000, signature)
+        ).to.be.revertedWith("invalid signature")
+
+        expect(0).to.equal(await token.balanceOf(holder.address))
+        await token.connect(holder).claim(deviceHash, 1668131000, 1668132000, signature)
+        expect(1).to.equal(await token.balanceOf(holder.address))
+
+        await expect(
+            token.connect(holder).claim(deviceHash, 1668131000, 1668132000, signature)
+        ).to.be.revertedWith("already claimed")
+
+        await expect(
+            token.connect(holder).transferFrom(holder.address, owner.address, 0)
+        ).to.be.revertedWith("SOULBOUND: Non-Transferable")
+    })
+})

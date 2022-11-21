660bc3a4c2882d8b849808ef7e1ccd9a88287c81
ququzone <xueping.yang@gmail.com>
2022-11-21 16:22:52 +0800

    format code

--- test/PebbleMultipleLocationNFT.spec.ts
+++ test/PebbleMultipleLocationNFT.spec.ts
@@ -36,7 +36,7 @@ describe("PebbleMultipleLocationNFT", function () {
     })
 
     it("check claim", async function () {
-        const deviceTimestamp = Math.floor(new Date().valueOf() / 1000);
+        const deviceTimestamp = Math.floor(new Date().valueOf() / 1000)
         const deviceHash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("12345"))
         const hash = ethers.utils.solidityKeccak256(
             ["address", "int256", "int256", "uint256", "bytes32", "uint256"],
@@ -47,7 +47,9 @@ describe("PebbleMultipleLocationNFT", function () {
         const signature = await signer.signMessage(messageHashBinary)
 
         await expect(
-            token.connect(owner).claim(120520000, 30400000, 100, deviceHash, deviceTimestamp, signature)
+            token
+                .connect(owner)
+                .claim(120520000, 30400000, 100, deviceHash, deviceTimestamp, signature)
         ).to.be.revertedWith("invalid signature")
 
         expect(0).to.equal(await token.balanceOf(holder.address))
@@ -57,7 +59,9 @@ describe("PebbleMultipleLocationNFT", function () {
         expect(1).to.equal(await token.balanceOf(holder.address))
 
         await expect(
-            token.connect(holder).claim(120520000, 30400000, 100, deviceHash, deviceTimestamp, signature)
+            token
+                .connect(holder)
+                .claim(120520000, 30400000, 100, deviceHash, deviceTimestamp, signature)
         ).to.be.revertedWith("already claimed")
     })
 

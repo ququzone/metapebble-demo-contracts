48d84fcb4ebcc6ae0757998ef48a104fb33a59ea
ququzone <xueping.yang@gmail.com>
2023-02-08 14:36:14 +0800

    add drop token example

---
+++ contracts/examples/MetapebbleVerifiedDrop.sol
@@ -0,0 +1,125 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+import "../interface/IMetapebbleDataVerifier.sol";
+import "../interface/IVerifyFeeSelector.sol";
+import "../interface/IVerifyFeeManager.sol";
+
+contract MetapebbleVerifiedDrop is Ownable, ReentrancyGuard {
+    event Claimed(address indexed holder, bytes32 indexed deviceHash, uint256 amount);
+
+    uint256 public AMOUNT_PER_DEVICE;
+    IMetapebbleDataVerifier public verifier;
+    // deviceHash => claimed address
+    mapping(bytes32 => address) internal _claimedDevices;
+
+    int256 private lat;
+    int256 private long;
+    uint256 private maxDistance;
+
+    constructor(
+        int256 _lat,
+        int256 _long,
+        uint256 _maxDistance,
+        address _verifier,
+        uint256 _amount
+    ) {
+        require(_amount > 0, "invalid amount");
+        lat = _lat;
+        long = _long;
+        maxDistance = _maxDistance;
+        verifier = IMetapebbleDataVerifier(_verifier);
+        AMOUNT_PER_DEVICE = _amount;
+    }
+
+    receive() external payable {
+        require(msg.value % AMOUNT_PER_DEVICE == 0, "invalid amount");
+    }
+
+    function refund() external onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    function claimable() external view returns (bool) {
+        return address(this).balance >= AMOUNT_PER_DEVICE;
+    }
+
+    function _claim(
+        address payable holder,
+        uint256 distance,
+        bytes32 deviceHash,
+        uint256 startTimestamp,
+        uint256 endTimestamp,
+        bytes memory signature,
+        uint256 value
+    ) internal virtual nonReentrant {
+        require(_claimedDevices[deviceHash] == address(0), "already claimed");
+        require(address(this).balance - msg.value >= AMOUNT_PER_DEVICE, "no fund");
+        require(distance <= maxDistance, "invalid location");
+        require(msg.value >= claimFee(), "miss verifier fee");
+
+        bytes32 digest = verifier.generateLocationDistanceDigest(
+            holder,
+            lat,
+            long,
+            distance,
+            deviceHash,
+            startTimestamp,
+            endTimestamp
+        );
+        require(verifier.verify{value: value}(digest, signature), "invalid signature");
+
+        _claimedDevices[deviceHash] = holder;
+        holder.transfer(AMOUNT_PER_DEVICE);
+        emit Claimed(holder, deviceHash, AMOUNT_PER_DEVICE);
+    }
+
+    function claim(
+        address payable holder,
+        uint256 distance,
+        bytes32 deviceHash,
+        uint256 startTimestamp,
+        uint256 endTimestamp,
+        bytes memory signature
+    ) external payable {
+        _claim(
+            holder,
+            distance,
+            deviceHash,
+            startTimestamp,
+            endTimestamp,
+            signature,
+            msg.value
+        );
+    }
+
+    function claim(
+        uint256 distance,
+        bytes32 deviceHash,
+        uint256 startTimestamp,
+        uint256 endTimestamp,
+        bytes memory signature
+    ) external payable {
+        _claim(
+            payable(msg.sender),
+            distance,
+            deviceHash,
+            startTimestamp,
+            endTimestamp,
+            signature,
+            msg.value
+        );
+    }
+
+    function claimFee() public view returns (uint256) {
+        return
+            IVerifyFeeManager(
+                IVerifyFeeSelector(verifier.verifyFeeSelector()).fetchVerifyFeeManager(
+                    address(this)
+                )
+            ).fee(address(this));
+    }
+}
---
+++ test/MetapebbleVerifiedDrop.spec.ts
@@ -0,0 +1,105 @@
+import { ethers } from "hardhat"
+import { expect } from "chai"
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
+
+import { MetapebbleDataVerifier } from "../typechain/contracts/MetapebbleDataVerifier"
+import { MetapebbleVerifiedDrop } from "../typechain/contracts/examples/MetapebbleVerifiedDrop"
+
+describe("MetapebbleVerifiedDrop", function () {
+    let verifier: MetapebbleDataVerifier
+    let token: MetapebbleVerifiedDrop
+    let owner: SignerWithAddress
+    let signer: SignerWithAddress
+    let holder: SignerWithAddress
+
+    before(async function () {
+        ;[owner, signer, holder] = await ethers.getSigners()
+
+        const feeManagerFactory = await ethers.getContractFactory("GeneralFeeManager")
+        const feeManager = await feeManagerFactory.deploy(1000)
+        const selectorFactory = await ethers.getContractFactory("VerifyFeeSelector")
+        const selector = await selectorFactory.deploy(feeManager.address)
+
+        const verifierFactory = await ethers.getContractFactory("MetapebbleDataVerifier")
+        verifier = (await verifierFactory.connect(owner).deploy()) as MetapebbleDataVerifier
+        await verifier.initialize([signer.address], selector.address)
+
+        const facory = await ethers.getContractFactory("MetapebbleVerifiedDrop")
+        token = (await facory.connect(owner).deploy(
+            120520000, // lat
+            30400000, // long
+            1000, // 1km
+            verifier.address, 
+            100
+        )) as MetapebbleVerifiedDrop
+    })
+
+    it("check basic info", async function () {
+        expect("100").to.equal(await token.AMOUNT_PER_DEVICE())
+        expect(false).to.equal(await token.claimable())
+    })
+
+    it("check claim", async function () {
+        const holderBalance = await ethers.provider.getBalance(holder.address)
+
+        const deviceHash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("12345"))
+        const hash = ethers.utils.solidityKeccak256(
+            ["address", "int256", "int256", "uint256", "bytes32", "uint256", "uint256"],
+            [holder.address, 120520000, 30400000, 100, deviceHash, 1668131000, 1668133000]
+        )
+        const messageHashBinary = ethers.utils.arrayify(hash)
+
+        const signature = await signer.signMessage(messageHashBinary)
+
+        await expect(
+            token
+                .connect(owner)
+                ["claim(uint256,bytes32,uint256,uint256,bytes)"]
+                (100, deviceHash, 1668131000, 1668133000, signature, {
+                    value: 1000,
+                })
+        ).to.be.revertedWith("no fund")
+
+        await expect(
+            owner.sendTransaction({to: token.address, value: 90})
+        ).to.be.revertedWith("invalid amount")
+        await owner.sendTransaction({to: token.address, value: 1000})
+
+        await expect(
+            token
+                .connect(owner)
+                ["claim(uint256,bytes32,uint256,uint256,bytes)"]
+                (100, deviceHash, 1668131000, 1668133000, signature, {
+                    value: 1000,
+                })
+        ).to.be.revertedWith("invalid signature")
+
+        expect(holderBalance).to.equal(await ethers.provider.getBalance(holder.address))
+        await token
+            .connect(owner)
+            ["claim(address,uint256,bytes32,uint256,uint256,bytes)"]
+            (holder.address, 100, deviceHash, 1668131000, 1668133000, signature, {
+                value: 1000,
+            })
+        expect(holderBalance.add(100)).to.equal(await ethers.provider.getBalance(holder.address))
+
+        expect(1000).to.equal(await ethers.provider.getBalance(verifier.address))
+
+        await verifier
+            .connect(owner)
+            .withdrawFee("0x0000000000000000000000000000000000000000", 1000)
+        expect(0).to.equal(await ethers.provider.getBalance(verifier.address))
+        expect(1000).to.equal(
+            await ethers.provider.getBalance("0x0000000000000000000000000000000000000000")
+        )
+
+        await expect(
+            token
+                .connect(holder)
+                ["claim(uint256,bytes32,uint256,uint256,bytes)"]
+                (100, deviceHash, 1668131000, 1668133000, signature, {
+                    value: 1000,
+                })
+        ).to.be.revertedWith("already claimed")
+    })
+})
